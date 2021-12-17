80ec10dd9fa9f56e8f7ad17d0ff17e132a69e8be
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2021-12-17 16:24:09 +0100

    Initial commit

---
+++ ProtobufSample/ProtobufSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProtobufSampleApp.swift
+//  ProtobufSample
+//
+//  Created by Stefan Lobbenmeier on 17.12.21.
+//
+//
+
+import SwiftUI
+
+@main
+struct ProtobufSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
